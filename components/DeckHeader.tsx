2327d6a434cf150007d61dd00ff6b7e11cbb30af
Jasper <jasper.apos@gmail.com>
2024-09-14 00:57:28 +0800

    style: deckheader margin top

--- components/DeckHeader.tsx
+++ components/DeckHeader.tsx
@@ -20,6 +20,7 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: "row",
     alignItems: "center",
+    marginTop: 20,
     justifyContent: 'space-between'
   },
   title: {
