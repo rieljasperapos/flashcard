ccfac690369984f11c890688d39b28de83ca1cc9
Jasper <jasper.apos@gmail.com>
2024-09-13 23:47:16 +0800

    feat: routing

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(home)` | `/(home)/` | `/_sitemap`;
+      StaticRoutes: `/` | `/(home)` | `/(home)/` | `/(home)/decks` | `/_sitemap` | `/decks`;
       DynamicRoutes: `/(home)/decks/${Router.SingleRoutePart<T>}` | `/decks/${Router.SingleRoutePart<T>}`;
       DynamicRouteTemplate: `/(home)/decks/[id]` | `/decks/[id]`;
     }
